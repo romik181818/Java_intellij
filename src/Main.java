83b68b4747555ad0e829a148c255142945ab6225
Rori6 <roma.lavroff@yandex.ru>
2025-05-10 14:27:52 +0400

    4commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ void main(){
 
     BigInteger a = new BigInteger("1000000000000000000000") ;
     System.out.println(a);
-    BigInteger result = a.multiply(new BigInteger("23"));
+    BigInteger result = a.multiply(new BigInteger("233"));
     System.out.println(result);
 }
