e9670c59418179d3997e8753a995c9758cf5c720
Rori6 <roma.lavroff@yandex.ru>
2025-05-10 14:23:31 +0400

    3 commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+void main(){
+
+
+    BigInteger a = new BigInteger("1000000000000000000000") ;
+    System.out.println(a);
+    BigInteger result = a.multiply(new BigInteger("23"));
+    System.out.println(result);
+}
